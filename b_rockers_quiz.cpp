a1ee5b6929fd0a10c4568815765f8dcf18717724
Natasha S Love <debora@inbox.ru>
2018-04-11 18:49:45 -0500

    this example is by the teacher

---
+++ b_rockers_quiz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+// the class will remember all data changes in the main
+class House {
+	
+		private:
+		
+		
+		public:
+		
+		
+};
+
+int main () {
+	
+	
+	
+	
+	
+	return 0;
+}
